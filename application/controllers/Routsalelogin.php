13482059c8a74b0edb6a21f1693f267f3188741e
geethusaroop <geethu.thachirethu@gmail.com>
2023-02-22 14:13:07 +0530

    routsale login

--- application/controllers/Routsalelogin.php
+++ application/controllers/Routsalelogin.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-class Branchlogin extends MY_Controller
+class Routsalelogin extends MY_Controller
 {
     public $table = 'admin_login';
-    public $page  = 'Branchlogin';
+    public $page  = 'Routsalelogin';
     public function __construct()
     {
         parent::__construct();
@@ -11,19 +11,19 @@ class Branchlogin extends MY_Controller
             redirect('/login');
         }
         $this->load->model('General_model');
-        $this->load->model('Branchlogin_model');
+        $this->load->model('Routsalelogin_model');
     }
     public function getdes()
     {
     $id = $this->input->post('id');
-    $data = $this->Branchlogin_model->getdes($id);
+    $data = $this->Routsalelogin_model->getdes($id);
     $json_data = json_encode($data);
     echo $json_data;
     }
     public function index()
     {
-        $template['body'] = 'Branchlogin/list';
-        $template['script'] = 'Branchlogin/script';
+        $template['body'] = 'Routsalelogin/list';
+        $template['script'] = 'Routsalelogin/script';
         $this->load->view('template', $template);
     }
     public function add()
@@ -32,16 +32,15 @@ class Branchlogin extends MY_Controller
         if ($this->form_validation->run() == FALSE) {
             $branch = ['branch_status'=> 1];
             $template['branch'] = $this->General_model->getall('tbl_branch',$branch);
-            $template['body'] = 'Branchlogin/add';
-            $template['script'] = 'Branchlogin/script';
+            $template['body'] = 'Routsalelogin/add';
+            $template['script'] = 'Routsalelogin/script';
             $this->load->view('template', $template);
         } else {
             $id = $this->input->post('id');
             $data = array(
-                'branch_id_fk' => $this->input->post('branch_id_fk'),
                 'user_name' => $this->input->post('user_name'),
                 'admin_password' => $this->input->post('admin_password'),
-                'user_type' => 'B',
+                'user_type' => 'RS',
                 'created_date' => date("Y-m-d"),
                 'updated_date' => date("Y-m-d"),
                 'user_status' => 1
@@ -49,29 +48,29 @@ class Branchlogin extends MY_Controller
             if ($id) {
                 $data['id'] = $id;
                 $result = $this->General_model->update($this->table, $data, 'id', $id);
-                $response_text = 'Branch Login Information  updated successfully';
+                $response_text = 'Routsale Login Information  updated successfully';
             } else {
                 $result = $this->General_model->add($this->table, $data);
-                $response_text = 'Branch Login Information added  successfully';
+                $response_text = 'Routsale Login Information added  successfully';
             }
             if ($result) {
                 $this->session->set_flashdata('response', "{&quot;text&quot;:&quot;$response_text&quot;,&quot;layout&quot;:&quot;topRight&quot;,&quot;type&quot;:&quot;success&quot;}");
             } else {
                 $this->session->set_flashdata('response', '{&quot;text&quot;:&quot;Something went wrong,please try again later&quot;,&quot;layout&quot;:&quot;bottomRight&quot;,&quot;type&quot;:&quot;error&quot;}');
             }
-            redirect('/Branchlogin/');
+            redirect('/Routsalelogin/');
         }
     }
     public function get()
     {
-        $this->load->model('Branchlogin_model');
+        $this->load->model('Routsalelogin_model');
         $param['draw'] = (isset($_REQUEST['draw'])) ? $_REQUEST['draw'] : '';
         $param['length'] = (isset($_REQUEST['length'])) ? $_REQUEST['length'] : '10';
         $param['start'] = (isset($_REQUEST['start'])) ? $_REQUEST['start'] : '0';
         $param['order'] = (isset($_REQUEST['order'][0]['column'])) ? $_REQUEST['order'][0]['column'] : '';
         $param['dir'] = (isset($_REQUEST['order'][0]['dir'])) ? $_REQUEST['order'][0]['dir'] : '';
         $param['searchValue'] = (isset($_REQUEST['search']['value'])) ? $_REQUEST['search']['value'] : '';
-        $data = $this->Branchlogin_model->getCompanyTable($param);
+        $data = $this->Routsalelogin_model->getCompanyTable($param);
         $json_data = json_encode($data);
         echo $json_data;
     }
@@ -90,14 +89,14 @@ class Branchlogin extends MY_Controller
         $response['layout'] = 'topRight';
         $data_json = json_encode($response);
         echo $data_json;
-        redirect('/Branchlogin/', 'refresh');
+        redirect('/Routsalelogin/', 'refresh');
     }
     public function edit($id)
     {
         $branch = ['branch_status'=> 1];
         $template['branch'] = $this->General_model->getall('tbl_branch',$branch);
-        $template['body'] = 'Branchlogin/add';
-        $template['script'] = 'Branchlogin/script';
+        $template['body'] = 'Routsalelogin/add';
+        $template['script'] = 'Routsalelogin/script';
         $template['records'] = $this->General_model->get_row($this->table, 'id', $id);
         $this->load->view('template', $template);
     }
